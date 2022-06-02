267f38470317c8a3b6d603ab90a2afa648f8a19e
CrystallineButterfly <95975209+CrystallineButterfly@users.noreply.github.com>
2022-06-02 11:12:52 +0100

    Add fix to linter and push all new file adds

---
+++ src/index.js
@@ -0,0 +1,8 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-plusplus */
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { v4 as uuidv4 } from 'uuid'; // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+import './styles/main.css';
+import './TodoList.js';
+
+uuidv4();
